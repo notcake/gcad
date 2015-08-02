e38ad1b62b931077de47f12ddeea7110b4817a52
notcake <cakenotfound@gmail.com>
2015-08-02 22:28:59 +0100

    [GCAD] Added Materials.CreateLitVertexColorMaterial () and Materials.CreateLitColorMaterial ().

--- lua/gcad/materials/utility.lua
+++ lua/gcad/materials/utility.lua
@@ -36,3 +36,37 @@ function GCAD.Materials.CreateColorMaterial (id, color)
 	
 	return material
 end
+
+function GCAD.Materials.CreateLitVertexColorMaterial (id, color)
+	local material = CreateMaterial (id, "VertexLitGeneric",
+		{
+			["$vertexcolor"] = 1,
+			["$vertexalpha"] = 1,
+			["$translucent"] = 1
+		}
+	)
+	
+	if color then
+		v, alpha = GLib.Color.ToVector (color, v)
+		material:SetVector ("$color", v)
+		material:SetFloat  ("$alpha", alpha)
+	end
+	
+	return material
+end
+
+function GCAD.Materials.CreateLitColorMaterial (id, color)
+	local material = CreateMaterial (id, "VertexLitGeneric",
+		{
+			["$translucent"] = 1
+		}
+	)
+	
+	if color then
+		v, alpha = GLib.Color.ToVector (color, v)
+		material:SetVector ("$color", v)
+		material:SetFloat  ("$alpha", alpha)
+	end
+	
+	return material
+end
