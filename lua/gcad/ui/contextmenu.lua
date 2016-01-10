b90ace055322d458f839c7dac80535b9144aa158
notcake <cakenotfound@gmail.com>
2016-01-10 21:26:58 +0000

    [GCAD] Disabled context menu override for everyone except !cake.

--- lua/gcad/ui/contextmenu.lua
+++ lua/gcad/ui/contextmenu.lua
@@ -1,3 +1,11 @@
+if not LocalPlayer or
+   not LocalPlayer () or
+   not LocalPlayer ():IsValid () or
+   not LocalPlayer ().SteamID or
+   LocalPlayer ():SteamID () ~= "STEAM_0:1:19269760" then
+	return
+end
+
 include ("contextmenu/selectionrenderer.lua")
 include ("contextmenu/eventedcollectionculler.lua")
 include ("contextmenu/contextmenucontextmenu.lua")
